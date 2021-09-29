500c369ae0990da27bcde12485e26d03ab0c2e4f
Kurnia Muhamad <omenkzz@gmail.com>
2021-09-29 21:55:17 +0700

    Add system test for charge

---
+++ source/spec/system/charges_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "Charges", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  it "three lists on the screen" do
+    visit "/charges"
+    expect(page).to have_content('Failed Charges')
+    expect(page).to have_content('Disputed Charges')
+    expect(page).to have_content('Successful Charges')
+  end
+
+  it "shows 10 items of successful charges item" do
+    visit "/charges"
+    expect(page).to have_xpath('//table/tbody[@id="successful-charges"]/tr', count: 10)
+  end
+
+  it "shows 5 items of failed charges item" do
+    visit "/charges"
+    expect(page).to have_xpath('//table/tbody[@id="failed-charges"]/tr', count: 5)
+  end
+
+  it "shows 5 items of disputed charges item" do
+    visit "/charges"
+    expect(page).to have_xpath('//table/tbody[@id="disputed-charges"]/tr', count: 5)
+  end
+end
